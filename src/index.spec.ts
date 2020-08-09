0f5f1d3f83a45b92540723a7c84ef9ea6b293185
Altay Aydemir <altay.aydemir@gmail.com>
2020-08-09 11:06:25 +0200

    update sanity checks

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,6 +3,7 @@ import * as PutioSocketClient from '.'
 test('API Sanity Checks', () => {
   expect(PutioSocketClient).toMatchInlineSnapshot(`
     Object {
+      "DEFAULT_API_URL": "https://socket.put.io/socket/sockjs",
       "EVENT_TYPE": Object {
         "APP_VERSION_UPDATED": "app_version_updated",
         "CONNECT": "connect",
@@ -30,6 +31,13 @@ test('API Sanity Checks', () => {
         "TRANSFER_UPDATE": "transfer_update",
         "USER_UPDATE": "user_update",
         "ZIP_CREATE": "zip_created",
+      },
+      "WEBSOCKET_CLOSEEVENT_CODE": Object {
+        "NORMAL_CLOSURE": 1000,
+        "UNAUTHORIZED": 4001,
+      },
+      "WEBSOCKET_ERROREVENT_CODE": Object {
+        "CONNECTION_REFUSED": "ECONNREFUSED",
       },
       "createPutioSocketClient": [Function],
     }
