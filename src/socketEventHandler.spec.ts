a055b09be317cce77d8cf528eb04dce35e14ed7f
Altay Aydemir <altay.aydemir@gmail.com>
2020-08-12 00:15:13 +0200

    add listener for heartbeat event

--- src/socketEventHandler.spec.ts
+++ src/socketEventHandler.spec.ts
@@ -5,12 +5,21 @@ import createSocketEventHandler from './socketEventHandler'
 
 describe('SocketEventHandler', () => {
   const mockToken = 'TOKEN'
-  const mockedWebSocket = mock<WebSocket>()
+  const mockedWebSocket = mock<WebSocket>({ readyState: 1 })
+  let webSocketEventMap: Record<string, () => void> = {}
   const mockedEmitter = mock<Emitter<EventMap>>()
   const mockedReconnect = jest.fn()
   const mockedOnConnect = jest.fn()
 
-  describe('events', () => {
+  beforeAll(jest.useFakeTimers)
+
+  beforeEach(() => {
+    webSocketEventMap = {}
+
+    mockedWebSocket.addEventListener = jest.fn((event, callback) => {
+      webSocketEventMap[event] = callback
+    }) as any
+
     createSocketEventHandler({
       token: mockToken,
       socket: mockedWebSocket,
@@ -18,18 +27,20 @@ describe('SocketEventHandler', () => {
       reconnect: mockedReconnect,
       onConnect: mockedOnConnect,
     })
+  })
 
-    afterEach(jest.clearAllMocks)
+  afterEach(jest.clearAllMocks)
 
-    it('handles connect event', () => {
-      const event = new Event('open')
-      mockedWebSocket.onopen && mockedWebSocket.onopen(event)
+  it('handles connect event', () => {
+    const event = new Event('open')
+    mockedWebSocket.onopen && mockedWebSocket.onopen(event)
 
-      expect(mockedEmitter.emit).toBeCalledWith('connect')
-      expect(mockedWebSocket.send).toBeCalledWith(mockToken)
-    })
+    expect(mockedEmitter.emit).toBeCalledWith('connect')
+    expect(mockedWebSocket.send).toBeCalledWith(mockToken)
+  })
 
-    it('handles message event with valid payload', () => {
+  describe('message event', () => {
+    it('with valid payload', () => {
       const event = new MessageEvent('user_update', {
         data: JSON.stringify({
           type: 'user_update',
@@ -44,21 +55,42 @@ describe('SocketEventHandler', () => {
       })
     })
 
-    it('ignores message event with invalid payload', () => {
+    it('with invalid payload', () => {
       jest.spyOn(console, 'warn').mockImplementation(() => null)
 
       const event = new MessageEvent('invalid_event', {
         data: JSON.stringify(null),
       })
+
       mockedWebSocket.onmessage && mockedWebSocket.onmessage(event)
 
       expect(mockedEmitter.emit).not.toBeCalled()
     })
+  })
 
-    it('handles error event', () => {
-      const event = new Event('error')
-      mockedWebSocket.onerror && mockedWebSocket.onerror(event)
-      expect(mockedEmitter.emit).toBeCalledWith('error')
+  it('handles error event', () => {
+    const event = new Event('error')
+    mockedWebSocket.onerror && mockedWebSocket.onerror(event)
+    expect(mockedEmitter.emit).toBeCalledWith('error')
+  })
+
+  describe('heartbeat -> close + reconnect flow', () => {
+    beforeEach(() => {
+      const event = new Event('open')
+      mockedWebSocket.onopen && mockedWebSocket.onopen(event)
+    })
+
+    it('assumes connection is problemmatic based on heartbeat event', () => {
+      jest.advanceTimersByTime(12000)
+      expect(mockedWebSocket.close).toBeCalled()
+      expect(mockedReconnect).toBeCalled()
+    })
+
+    it('does not try to close websocket connection if it is already closing', () => {
+      ;(mockedWebSocket as any).readyState = 2
+      jest.advanceTimersByTime(12000)
+      expect(mockedWebSocket.close).not.toBeCalled()
+      expect(mockedReconnect).toBeCalled()
     })
   })
 })
--- src/socketEventHandler.ts
+++ src/socketEventHandler.ts
@@ -5,13 +5,11 @@ import {
   WEBSOCKET_ERROREVENT_CODE,
 } from './constants'
 
-type SocketHandlerParams = {
-  token: string
-  socket: WebSocket
-  eventEmitter: Emitter<EventMap>
-  reconnect: () => void
-  onConnect: () => void
-}
+const HEARTBEAT_INTERVAL_IN_SECONDS = 10
+const HEARTBEAT_LATENCY_ASSUMPTION_IN_SECONDS = 2
+const HEARTBEAT_TIMER =
+  (HEARTBEAT_INTERVAL_IN_SECONDS + HEARTBEAT_LATENCY_ASSUMPTION_IN_SECONDS) *
+  1000
 
 const createSocketEventHandler = ({
   token,
@@ -19,8 +17,28 @@ const createSocketEventHandler = ({
   eventEmitter,
   reconnect,
   onConnect,
-}: SocketHandlerParams) => {
+}: {
+  token: string
+  socket: WebSocket
+  eventEmitter: Emitter<EventMap>
+  reconnect: () => void
+  onConnect: () => void
+}) => {
+  let pingTimeout: NodeJS.Timeout
+
+  const setPingTimeout = () => {
+    clearTimeout(pingTimeout)
+    pingTimeout = setTimeout(closeConnectionAndReconnect, HEARTBEAT_TIMER)
+  }
+
+  const closeConnectionAndReconnect = () => {
+    if (socket.readyState < 2) socket.close()
+    clearTimeout(pingTimeout)
+    reconnect()
+  }
+
   socket.onopen = () => {
+    setPingTimeout()
     socket.send(token)
     eventEmitter.emit(EVENT_TYPE.CONNECT)
     onConnect()
@@ -34,7 +52,7 @@ const createSocketEventHandler = ({
       event.code < 4000 &&
       event.code !== WEBSOCKET_CLOSEEVENT_CODE.NORMAL_CLOSURE
     ) {
-      reconnect()
+      closeConnectionAndReconnect()
     }
   }
 
@@ -45,7 +63,7 @@ const createSocketEventHandler = ({
       (event as Record<string, any>).code ===
       WEBSOCKET_ERROREVENT_CODE.CONNECTION_REFUSED
     ) {
-      reconnect()
+      closeConnectionAndReconnect()
     }
   }
 
@@ -57,6 +75,8 @@ const createSocketEventHandler = ({
       console.warn(`Could not deserialize message payload: `, e)
     }
   }
+
+  socket.addEventListener('heartbeat', () => setPingTimeout())
 }
 
 export default createSocketEventHandler
